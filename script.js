3806b2e71a46cd0a12da0fc129d1f7005ed853f3
Angelo31294 <luongoangelo1@gmail.com>
2022-10-20 16:30:08 +0200

    Agg. risultato finale con 2 decimali

--- script.js
+++ script.js
@@ -8,20 +8,23 @@ console.log( typeof age, age);
 
 // 3. Calcolo del prezzo del biglietto (0,21€)
 const pricekm = Number(0.21);
-let initialPrice = Number(km * pricekm);
+const initialPrice = Number(km * pricekm);
 console.log(typeof initialPrice, initialPrice);
 
-//   3a SE l'utente ha un età compresa tre i 18 e i 64 anni (compresi) non applico nessuno sconto
-if( age > 17 && age < 65 ) {
-    let totPrice = Number(initialPrice * 1);
-    console.log(typeof totPrice, totPrice)
-} else if ( age < 18 ) {
-    let totPrice = Number(initialPrice * 0.2);
-    console.log(typeof totPrice, totPrice)
-} else if ( age > 64 ) {
-    let totPrice = Number(initialPrice * 0.4);
-    console.log(typeof totPrice, totPrice)
+if( age > 17 && age < 65 ) {                //  3a SE l'utente ha un età compresa tre i 18 e i 64 anni (compresi) non applico nessuno sconto
+    const fullPrice = Number(initialPrice * 1);
+    console.log(typeof fullPrice, fullPrice);
+} else if ( age < 18 ) {                   //   3b SE l'utente ha un'età < 18 anni (minorenne) applico il 20% di sconto
+    const youngDiscount = Number(initialPrice * 0.2);
+    const youngPrice = Number(initialPrice - youngDiscount);
+    const twoDecimalyoungPrice = youngPrice.toFixed(2);   // 4. Output del prezo finale (con massimo due decimali)
+    console.log(typeof twoDecimalyoungPrice, twoDecimalyoungPrice);
+} else if ( age > 64 ) {                   //   3c ALTRIMENTI applico il 40% di sconto SE l'utente ha un'età > 64 anni
+    const oldDiscount = Number(initialPrice * 0.4);
+    const oldPrice = Number(initialPrice - oldDiscount);
+    const twoDecimaloldPrice = oldPrice.toFixed(2);   // 4. Output del prezo finale (con massimo due decimali)
+    console.log(typeof twoDecimaloldPrice, twoDecimaloldPrice);
 }
-//   3b SE l'utente ha un'età < 18 anni (minorenne) applico il 20% di sconto
-//   3c ALTRIMENTI applico il 40% di sconto SE l'utente ha un'età > 64 anni
-// 4. Output del prezo finale (con massimo due decimali)
+
+
+
