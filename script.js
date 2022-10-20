ada8a166d6bf724a7306c9614613b333b5619eb2
Angelo31294 <luongoangelo1@gmail.com>
2022-10-20 14:22:09 +0200

    Scaffolder

---
+++ script.js
@@ -0,0 +1,7 @@
+// 1. Chiedo all'utente quanti chilometri dovrà percorrere
+// 2. Chiedo all'utente la sua età
+// 3. Calcolo del prezzo del biglietto (0,21€)
+//   3a SE l'utente ha un età compresa tre i 18 e i 64 anni (compresi) non applico nessuno sconto
+//   3b SE l'utente ha un'età < 18 anni (minorenne) applico il 20% di sconto
+//   3c ALTRIMENTI applico il 40% di sconto se l'utente ha un'età > 64 anni
+// 4. Output del prezo finale (con massimo due decimali)
