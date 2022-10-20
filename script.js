50bbc16cc622596dc8ca6b2ff32897d4dc78a317
Angelo31294 <luongoangelo1@gmail.com>
2022-10-20 17:26:03 +0200

    Agg. stampa del prezzo finale - Fine

--- script.js
+++ script.js
@@ -17,14 +17,21 @@ if( age > 17 && age < 65 ) {                //  3a SE l'utente ha un età compre
 } else if ( age < 18 ) {                   //   3b SE l'utente ha un'età < 18 anni (minorenne) applico il 20% di sconto
     const youngDiscount = Number(initialPrice * 0.2);
     const youngPrice = Number(initialPrice - youngDiscount);
-    const twoDecimalyoungPrice = youngPrice.toFixed(2);   // 4. Output del prezo finale (con massimo due decimali)
-    console.log(typeof twoDecimalyoungPrice, twoDecimalyoungPrice);
+    var totPrice = youngPrice.toFixed(2);   // 4. Output del prezo finale (con massimo due decimali)
+    console.log(typeof totPrice, totPrice);
 } else if ( age > 64 ) {                   //   3c ALTRIMENTI applico il 40% di sconto SE l'utente ha un'età > 64 anni
     const oldDiscount = Number(initialPrice * 0.4);
     const oldPrice = Number(initialPrice - oldDiscount);
-    const twoDecimaloldPrice = oldPrice.toFixed(2);   // 4. Output del prezo finale (con massimo due decimali)
-    console.log(typeof twoDecimaloldPrice, twoDecimaloldPrice);
+    var totPrice = oldPrice.toFixed(2);   // 4. Output del prezo finale (con massimo due decimali)
+    console.log(typeof totPrice, totPrice);
 }
+console.log(
+    `
+    ${totPrice}
+    `
+);
 
+// Stampa del prezzo finale
+document.querySelector("h1").innerHTML = (`Pezzo totale del biglietto è ${totPrice}`);   
 
 
