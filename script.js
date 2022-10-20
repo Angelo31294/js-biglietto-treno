d8cdc38f9734423a67d0580fe7e165a6683d3666
Angelo31294 <luongoangelo1@gmail.com>
2022-10-20 14:31:05 +0200

    Agg. richiesta dei chilometri da percorrere

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // 1. Chiedo all'utente quanti chilometri dovrà percorrere
+const km = Number(prompt("Inserire i chilometri da percorrere"));
+console.log( typeof km, km)
 // 2. Chiedo all'utente la sua età
 // 3. Calcolo del prezzo del biglietto (0,21€)
 //   3a SE l'utente ha un età compresa tre i 18 e i 64 anni (compresi) non applico nessuno sconto
